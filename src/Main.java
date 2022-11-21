9f5de488c6569f6e28901d5a9923200b618f8c87
Marcus McClatchy <marcusmc10101@gmail.com>
2022-11-21 12:36:18 -0800

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileReader fr = new FileReader("example dfa.txt");
+        File file = new File("example dfa.txt");
 
-        // Declaring loop variable
-        int i;
-        // Holds true till there is nothing to read
-        while ((i = fr.read()) != -1)
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
-            // Print all the content of a file
-            System.out.print((char)i);
+
+        String st;
+        Vector<Vector<String>> tableVect = new Vector<>();
+        while ((st = br.readLine()) != null) {
+            System.out.println(st);
+            String[] currencies = st.split(" ");
+            Vector<String> tempArray = new Vector<>();
+            for(int i = 0; i < currencies.length; i++){
+                tempArray.add(currencies[i]);
+            }
+            tableVect.add(tempArray);
+        }
+        System.out.println(tableVect);
+
     }
 }
